57effc6ad09462a3664d167f5ca77b6dcd3d042a
Ivy Vetor <ivyvetor@gmail.com>
2016-06-17 14:04:37 -0400

    Update ArraysAndStrings.rb

--- ArraysAndStrings.rb
+++ ArraysAndStrings.rb
@@ -1,4 +1,5 @@
 # 1.1 Implement an algorithm to determine if a string has all unique characters.  What if you cannot use additional data structures?
+#Test cases => "Lemon" = true, "Happy" = false, "Loly" = false
 
 def unique?(string)
   arr = string.split('')
@@ -18,4 +19,26 @@ def unique?(string)
     end
   end
   return true
+end
+
+# 1.2 Implement a function void reverse(char* str) in C or C++ which reverses a null-terminated string.
+# REVISED question for Ruby: reverse string without using the reverse function
+#Test case: "Hello" = "olleH"
+
+def reverse(string)
+  if string == String #check edge case to make sure input is a string
+  	arr = string.split('')
+  	counter = 1
+  	new_str = []
+  
+  	while counter <= arr.length do
+    	new_str << arr[-counter]
+    	counter += 1
+  	end
+  
+  	return new_str.join('')
+  	
+  else
+  	return "not a string"
+  end
 end
